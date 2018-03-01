25218e31694074d922175d3258cabc4e4e2dfc5c
greeny3ny <craig-little@live.co.uk>
2018-03-01 02:07:04 +0000

    Code clean, 2d play, collision change
    - Code cleaned up!
    - 2D mode able to play (kinda)
    - Collision detection updated (still in works tho)

--- enemy.js
+++ enemy.js
@@ -1,21 +1,28 @@
-var eX = -0.3;
+//local
+var eX = -0.3; //temp val
+var eY = 1.37; //temp val
+
+console.log("Enemy.js loaded!");
 
 function enemy(){
 
 	console.log("Enemy created");
 	this.eX = getEX;
-	var eY = FLOOR;
-			
+	this.eY = getEY;
+	
+	
 	var en = document.createElement('a-box');
 	en.setAttribute('position',eX + ' ' + eY + ' 0.02');
 	en.setAttribute('scale', SCALE +' ' + SCALE +' '+SCALE);
-	//en.
 			
 	scene.appendChild(en);
-	
 	
 }
 
 function getEX(){
 	return eX;
+}
+
+function getEY(){
+	return eY;
 }
