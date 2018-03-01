bd702deed937cdeb2bf26a18e196a249674c4808
greeny3ny <craig-little@live.co.uk>
2018-03-01 20:24:52 +0000

    shift collision to own script

---
+++ collision.js
@@ -0,0 +1,36 @@
+console.log("collision.js loaded!");
+
+function detectCollision(){
+	
+	for (i in enemies){
+	var eX = enemies[i].eX;
+	var eY = enemies[i].eY;
+	const eZ = 0.02;
+			
+	var xCol = (playerX-0.0375 < (eX+0.0375) && playerX+0.0375 > (eX-0.0375));
+	var yCol = (playerY-0.0375 < (eY+0.0375) && playerY+0.0375 > (eY-0.0375));
+			
+	if (xCol && yCol){
+		if (playerY > eY){
+			console.log("player win");
+			enemies[i].en.setAttribute('color', 'red');
+			//remove enemy
+			//drop egg
+			scene.remove(enemies[i].en);
+			console.log(i+"destroy");
+			enemies[i].destroy();
+			enemies.pop(i);
+			birdsAlive --;
+			//enemy = null;
+		}
+		else if (eY > playerY){
+			console.log("enemy win");
+			enemies[i].en.setAttribute('color', 'green');
+		}
+		else {
+			console.log("bump");
+		}
+
+	}
+	}	
+}
