e7c399be7d38ffd8499505b931d124341584afb9
greeny3ny <craig-little@live.co.uk>
2018-03-03 16:41:15 +0000

    03/03/2018 push
    vague representations of "AI" forming in enemies...
    collision on enemies and player
    enemies should bounce off each other (still buggy)
    reset button in works but not looking good :/
    cleaned some code somewhere probably

--- js/enemy.js
+++ js/enemy.js
@@ -1,13 +1,27 @@
 console.log("enemy.js loaded!");
 
-function enemy(x, y){
+function enemy(x, y, dir, i){
 
 	var en = document.createElement('a-box');
 
 	var eX = x; //temp val
 	var eY = y; //temp val 1.37
+	var index = i;
+	
+	var flapTime = 600;
 	
 	var destroyed = false;
+	
+	var direction;
+	var collided = false;
+	
+	if (dir == 1){
+		direction="left";
+	}
+	else
+	{
+		direction="right";
+	}
 	
 	console.log("Enemy created");
 	//vars
@@ -15,11 +29,20 @@ function enemy(x, y){
 	this.eY = eY;
 	this.en = en;
 	this.dead = destroyed;
+	this.index = i;
+	this.direction = direction;
+	this.collide = collided;
+	
+	console.log(this.index);
+	
+	enPhys[this.index] = new physics(x, y);
 	
 	//functions
 	this.destroy = setDead;
 	this.setPos = setPos;
-	
+	this.move = movement;
+	this.col = enemyCollision;
+
 	en.setAttribute('scale', SCALE +' ' + SCALE +' '+SCALE);
 			
 	scene.appendChild(en);
@@ -31,12 +54,65 @@ function setPos(){
 }
 
 function setDead(i){
-	enemies[i].en.setAttribute('color', 'red');
-	scene.remove(enemies[i].en);
-	birdsAlive --;
 	
 	this.dead = true;
 	
 	console.log("enemy destroyed" + i);
 	console.log("dropping egg!");
 }
+
+function movement(){
+	
+	if (!this.dead){
+		enPhys[this.index].mtmCng(this.direction);
+		enPhys[this.index].fall();
+		
+		this.col();
+		//this.setFlapTime();
+		
+		this.eX = enPhys[this.index].moveX();
+		for (j in platforms){
+			platforms[j].deCol(this.eX, this.eY, enPhys[this.index]);
+		}
+	}
+	else{
+		this.eX = 100;
+		this.eY = 100;
+	}
+	this.setPos();
+}
+
+function enemyCollision(){
+	//console.log("test");
+	const bumpVal = -0.5;
+
+	for (i in enemies){
+		if (i == this.index){
+			//comparing against self so do nothing
+		} else {
+			var cE = detectEnemyCollision(this.eX, this.eY,i); //refactor!
+			if (cE == "win" || cE =="lose" || cE=="bump"){
+				if (!this.collide){
+					enPhys[this.index].Xmom = -enPhys[this.index].Xmom;
+					if (this.direction == "right"){
+						this.direction = "left";
+						this.eX = this.eX-bumpVal;
+						//enemies[i].eX = enemies[i]+bumpVal;
+					}
+					else
+					{
+						this.direction="right";
+						this.eX = this.eX+bumpVal;
+						//enemies[i].eX = enemies[i]+bumpVal;
+					}
+				}
+				this.collide = true;
+			}
+			else{
+				this.collide = false;
+			}
+		}	
+	}
+}//end function
+
+			
--- js/platform.js
+++ js/platform.js
@@ -1,11 +1,12 @@
 console.log("platform.js loaded!");
 
-function platform(w, x, y){
+function platform(w, x, y, i){
 
 	console.log("platform spawned");
 	
 	var width = w;
 	var height = 2;
+	var id = i;
 	
 	var platX = x;
 	var platY = y;
@@ -14,6 +15,10 @@ function platform(w, x, y){
 	this.width = width;
 	this.platX = platX;
 	this.platY = platY;
+	this.id = id;
+	
+	//functions
+	this.deCol = detectPlatformCollision;
 	
 	var platObj = document.createElement('a-box');
 	platObj.setAttribute('scale', w + ' 0.05 0.1');
@@ -21,4 +26,32 @@ function platform(w, x, y){
 	platObj.setAttribute('color', 'brown');
 	
 	scene.appendChild(platObj);
+}
+
+var touching = [];
+function detectPlatformCollision(x, y, phys){
+	var widthHalf = this.width/2;
+	var hitFloor = (y-HALFSCALE < (this.platY+0.025) && y-HALFSCALE > (this.platY-0.025));
+	var hitRoof  = (y+HALFSCALE > (this.platY-0.025) && y+HALFSCALE < (this.platY+0.025)); //&& playerY+HALFSCALE < (platY-0.025)
+	var xCol = (x-HALFSCALE < (this.platX+widthHalf) && x+HALFSCALE > (this.platX-widthHalf));
+	
+	touching[this.id] = ((hitRoof||hitFloor) && xCol);
+
+	if (hitRoof && xCol){
+		phys.roof = this.platY-0.05;
+	}
+	else{
+		if (!(touching[0] || touching[1] || touching[2] || touching[3])){
+			phys.roof = 2.3;
+		}
+	}
+	
+	if (hitFloor && xCol){
+		phys.floor = this.platY+0.05;
+	}
+	else{
+		if (!(touching[0] || touching[1] || touching[2] || touching[3])){
+			phys.floor = 1.3;
+		}
+	}
 }
